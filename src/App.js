c22b1202b1461f44c1d83c6c985dfef196a940b2
Shawn <krassnariose@gmail.com>
2026-05-06 05:16:48 +0200

    connect app components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import SongPage from "./SongPage";
 import './App.css';
+import InfoPage from "./InfoPage";
+import WillNeed from "./WillNeed";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/song/:id" element={<SongPage />} />
+        <Route path="/info/:id" element={<InfoPage />} /> 
+        <Route path="~:id" element={<WillNeed />} /> 
+      </Routes>
+    </Router>
+ );
 }
 
 export default App;
+
+
